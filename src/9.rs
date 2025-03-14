48621e3bc6ae28bde8bc53c5e6b9c867b78d3e2b
pasternakpanetta678 <pasternakpanetta678@gmail.com>
2025-03-14 00:42:16 +0000

    Add 9.rs

---
+++ src/9.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+    
+    if x < y {
+        println!("{} is less than {}", x, y);
+    } else {
+        println!("{} is greater than or equal to {}", x, y);
+    }
+}
