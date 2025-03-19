aa8c5cc305a6af907196f40ad6ad6466b14f7348
pasternakpanetta678 <pasternakpanetta678@gmail.com>
2025-03-19 06:55:16 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,8 @@
+import std::collections::HashMap;
+fn main() {
+  let mut map = HashMap::new();
+  map.insert("key1", "value1");
+  map.insert("key2", "value2");
+
+  println!("{:?}", map); // prints `{"key1": "value1", "key2": "value2"}`
+}
